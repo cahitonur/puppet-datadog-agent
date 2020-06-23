3e71a8004aa0bcbd6dc0c78223ba729103a4dfef
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2020-06-23 12:48:23 +0200

    Add support for SUSE Supported versions are the same as the zypprepo dependency This also adds Kitchen tests and a SUSE test environment

---
+++ manifests/suse.pp
@@ -0,0 +1,58 @@
+# Class: datadog_agent::suse
+#
+# This class contains the DataDog agent installation mechanism for SUSE distributions
+#
+
+class datadog_agent::suse(
+  Integer $agent_major_version = $datadog_agent::params::default_agent_major_version,
+  String $agent_version = $datadog_agent::params::agent_version,
+  String $release = $datadog_agent::params::apt_default_release,
+  Optional[String] $agent_repo_uri = undef,
+) inherits datadog_agent::params {
+
+  $keys = [
+    'https://yum.datadoghq.com/DATADOG_RPM_KEY.public',
+    'https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public',
+    'https://yum.datadoghq.com/DATADOG_RPM_KEY_20200908.public',
+  ]
+
+  case $agent_major_version {
+      5 : { fail('Agent v5 package not available in SUSE') }
+      6 : {
+        $defaulturl = "https://yum.datadoghq.com/stable/6/${::architecture}/"
+        $gpgkeys = $keys
+      }
+      7 : {
+        $defaulturl = "https://yum.datadoghq.com/stable/7/${::architecture}/"
+        $gpgkeys = $keys[1,2]
+      }
+      default: { fail('invalid agent_major_version') }
+  }
+
+  if ($agent_repo_uri != undef) {
+    $baseurl = $agent_repo_uri
+  } else {
+    $baseurl = "https://yum.datadoghq.com/suse/${release}/${agent_major_version}/${::architecture}"
+  }
+
+  package { 'datadog-agent-base':
+    ensure => absent,
+    before => Package[$datadog_agent::params::package_name],
+  }
+
+  zypprepo { 'datadog':
+    baseurl       => $baseurl,
+    enabled       => 1,
+    autorefresh   => 1,
+    name          => 'datadog',
+    gpgcheck      => 1,
+    repo_gpgcheck => 0,
+    gpgkey        => join($gpgkeys, "\n       "),
+    keeppackages  => 1,
+  }
+
+  package { $datadog_agent::params::package_name:
+    ensure  => $agent_version,
+  }
+
+}
